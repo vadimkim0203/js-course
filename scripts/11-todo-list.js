9e1e005c5fb5266b170d77e9498a08c3552d3dbc
Vadim Kim <vkim0203@gmail.com>
2024-01-05 11:50:56 +0900

    js-course 05.01

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -15,13 +15,13 @@ function renderTodoList () {
   const { name, dueDate } = todoObject;
 
   const html = `
-    <p>
-      ${name} ${dueDate}
-      <button onclick="
-        todoList.splice(${i}, 1);
-        renderTodoList();
-        ">Delete</button> 
-    </p>`;
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+      " class="delete-todo-button">Delete</button> 
+    `;
   todoListHTML += html;
   }
 
