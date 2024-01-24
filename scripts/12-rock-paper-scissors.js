4ff566e9fc483cc8365d20e9cef573fa175a9c0d
Vadim Kim <vkim0203@gmail.com>
2024-01-24 21:26:44 +0900

    js-course-24.1.24

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -19,10 +19,11 @@ if (!score) {
 let isAutoPlaying = false;
 let intervalId;
 
-
+// const autoPlay = () => {
+// };
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function() {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
   
@@ -33,6 +34,31 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+document.querySelector('.js-rock-button')
+.addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button')
+.addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button')
+.addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper')
+  } else if (event.key === 's') {
+    playGame('scissors')
+  }
+});
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
